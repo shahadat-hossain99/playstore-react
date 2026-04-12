69f092a175e1d2d6ab5d600a4dc25fe41c20bc20
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-04-12 21:56:28 +0600

    useEffect+ fallback + setTimeOut + slic in map to show less data

--- src/components/homepage/TrendingApps.jsx
+++ src/components/homepage/TrendingApps.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AppCard from "../ui/AppCard";
+import { HashLoader } from "react-spinners";
+import { Link } from "react-router";
 // import { use } from "react";
 
 // const appsPromise = fetch("/data.json").then((res) => res.json());
@@ -9,18 +11,23 @@ const TrendingApps = () => {
   // console.log(apps);
 
   const [apps, setApps] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("/data.json");
       const data = await res.json();
       console.log(data);
-      setApps(data);
+      setTimeout(() => {
+        setApps(data);
+        setLoading(false);
+      }, 4000);
     };
     fetchData();
   }, []);
 
   console.log(apps, "array of apps");
+  console.log(loading, "array of apps");
 
   return (
     <div className=" container mx-auto">
@@ -32,10 +39,31 @@ const TrendingApps = () => {
       </div>
 
       <h3> Total Apps :{apps.length}</h3>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {apps.map((app, ind) => {
-          return <AppCard key={ind} app={app}></AppCard>;
-        })}
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <HashLoader
+            color="#c10fc0"
+            cssOverride={{}}
+            loading
+            size={100}
+            speedMultiplier={0.8}
+          />
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {apps.slice(0, 9).map((app, ind) => {
+            return <AppCard key={ind} app={app}></AppCard>;
+          })}
+        </div>
+      )}
+
+      <div className="text-center">
+        <Link to="/apps">
+          {" "}
+          <button className=" btn bg-linear-to-r from-[#b944c4] to-[#9514FA] text-white mt-5">
+            Show All
+          </button>
+        </Link>
       </div>
     </div>
   );
