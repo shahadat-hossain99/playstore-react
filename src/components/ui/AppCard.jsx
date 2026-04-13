e9321ea9c45e41ad1166ab81f065fa3f3aa28a87
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-04-13 08:07:07 +0600

    appDetais loaded and footer added

--- src/components/ui/AppCard.jsx
+++ src/components/ui/AppCard.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { Link } from "react-router";
 
 const AppCard = ({ app }) => {
   return (
-    <div className="card bg-white p-5  shadow-sm mx-2">
-      <figure>
-        <img
-          className="object-contain h-54 w-auto rounded-full"
-          src={app.image}
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body p-2">
-        <h2 className="card-title align-bottom">{app.title}</h2>
+    <Link to={`/apps/${app.id}`}>
+      <div className="card bg-white p-5  shadow-sm mx-2">
+        <figure>
+          <img
+            className="object-contain h-54 w-auto rounded-full"
+            src={app.image}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body p-2">
+          <h2 className="card-title align-bottom">{app.title}</h2>
 
-        <div className="flex justify-between items-center pt-4 mt-auto">
-          <span className="font-bold btn btn-soft btn-success">
-            <LuDownload /> {app.downloads}{" "}
-          </span>
-          <span className="btn btn-warning font-bold btn-soft">
-            <FaStar />
-            {app.ratingAvg}
-          </span>
+          <div className="flex justify-between items-center pt-4 mt-auto">
+            <span className="font-bold btn btn-soft btn-success">
+              <LuDownload /> {app.downloads}{" "}
+            </span>
+            <span className="btn btn-warning font-bold btn-soft">
+              <FaStar />
+              {app.ratingAvg}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/appDetails/AppDetails.jsx
+++ src/pages/appDetails/AppDetails.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useParams } from "react-router";
 import useApps from "../../hooks/useApps";
+import DImg from "../../assets/images/icon-downloads.png";
+import RImg from "../../assets/images/icon-ratings.png";
+import ReImg from "../../assets/images/icon-review.png";
+import { HashLoader } from "react-spinners";
+import NotFound from "../notFoundPage/NotFoundPage";
 
 const AppDetails = () => {
   const { id } = useParams();
@@ -8,12 +13,78 @@ const AppDetails = () => {
   const { apps, loading } = useApps();
   //   console.log(apps, loading);
 
-  const expectedApp = apps.find((app) => app.id === parseInt(id));
+  const expectedApp = apps.find((app) => app.id == id);
   console.log(expectedApp);
 
+  // 2. Add this check! If it's still loading or app isn't found yet, show a loader
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-[60vh]">
+        <HashLoader
+          color="#c10fc0"
+          cssOverride={{}}
+          loading
+          size={100}
+          speedMultiplier={0.8}
+        />
+      </div>
+    );
+  }
+
+  // 3. Handle the case where the ID is wrong or app doesn't exist
+  if (!expectedApp) {
+    return <NotFound></NotFound>;
+  }
+
   return (
-    <div>
-      <h3>AppDetails</h3>
+    <div className="container mx-auto mt-20">
+      <div className="flex gap-10  items-center">
+        <div>
+          <img className="w-auto h-80" src={expectedApp.image} alt="" />
+        </div>
+        <div className="">
+          <div className="space-y-3">
+            <h2 className="text-4xl font-bold">{expectedApp.title}</h2>
+            <p className="text-gray-500">
+              Developed by
+              <span className="text-purple-400 font-bold">
+                {" "}
+                {expectedApp.companyName}
+              </span>
+            </p>
+          </div>
+          <div className="divider"></div>
+          <div>
+            <div className="flex gap-10">
+              <div>
+                <img src={DImg} alt="" />
+                <p>Downloads</p>
+                <h3 className="font-bold text-4xl">{expectedApp.downloads}</h3>
+              </div>
+
+              <div>
+                <img src={RImg} alt="" />
+                <p>Average Ratings</p>
+                <h3 className="font-bold text-4xl">{expectedApp.ratingAvg}</h3>
+              </div>
+
+              <div>
+                <img src={ReImg} alt="" />
+                <p>Total Reviews</p>
+                <h3 className="font-bold text-4xl">{expectedApp.reviews}</h3>
+              </div>
+            </div>
+            <button className="btn bg-purple-400 mt-4 text-white">
+              Install Now ({expectedApp.size} MB)
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+      <div></div>
+      <div className="divider"></div>
+      <p className="text-gray-500 font-medium">{expectedApp.description}</p>
     </div>
   );
 };
